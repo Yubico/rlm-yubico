dffefeda9f140d66851bd7dffe29540641b4df3e
Dain Nilsson <dain@yubico.com>
2013-02-19 09:39:35 +0100

    Added lookup_username login without a username.

--- yubikey.pl
+++ yubikey.pl
@@ -18,6 +18,8 @@ use constant	RLM_MODULE_NUMCODES=>  9;#  /* How many return codes there are */
 our $id_len = 12;
 our $verify_url = "http://127.0.0.1/wsapi/2.0/verify";
 our $client_id = 1;
+our $api_key = "";
+our $allow_userless_login = 0;
 
 # Load user configuration
 do "/etc/yubico/rlm/ykrlm-config.cfg";
@@ -59,11 +61,17 @@ sub authorize {
 	}
 
 	my $username = $RAD_REQUEST{'User-Name'};
-
+	
 	# Handle OTP
 	if($otp eq '') {
 		# No OTP
-		if(requires_otp($username)) {
+		if($username eq '') {
+			# No OTP or username, reject
+			&radiusd::radlog(1, "Reject: No username or OTP");
+			$RAD_REPLY{'Reply-Message'} = "Missing username and OTP!";
+			return RLM_MODULE_REJECT;
+
+		} elsif(requires_otp($username)) {
 			$RAD_REPLY{'State'} = encrypt_password($RAD_REQUEST{'User-Password'});
 			$RAD_REPLY{'Reply-Message'} = "Please provide YubiKey OTP";
 			$RAD_CHECK{'Response-Packet-Type'} = "Access-Challenge";
@@ -76,13 +84,19 @@ sub authorize {
 	} elsif(validate_otp($otp)) {
 		my $publicId = substr($otp, 0, $id_len);
 
+		#Lookup username if needed/allowed.
+		if($username eq '' and $allow_userless_login) {
+			$username = lookup_username($publicId);
+			$RAD_REQUEST{'User-Name'} = $username;
+		}
+
 		if(otp_belongs_to($publicId, $username)) {
 			&radiusd::radlog(1, "$username has valid OTP: $otp");
 			return RLM_MODULE_OK;
 		} elsif(can_provision($publicId, $username)) {
 			&radiusd::radlog(1, "Attempt to provision $publicId for $username post authentication");
 			$RAD_CHECK{'YubiKey-Provision'} = $publicId;
-			return RLM_MODULE_UPDATED;
+			return RLM_MODULE_UPDATED;	
 		} else {
 			&radiusd::radlog(1, "Reject: $username using valid OTP from foreign YubiKey: $publicId");
 			$RAD_REPLY{'Reply-Message'} = "Invalid OTP!";
@@ -159,6 +173,13 @@ sub provision {
 	
 	#TODO: Insert provisioning logic here
 	&radiusd::radlog(1,"Provisioned $publicId to $username");
+}
+
+sub lookup_username {
+	my($publicId) = @_;
+
+	#TODO: Lookup username for OTP
+	return "baduser";
 }
 
 # Encrypts a password using an instance specific key
