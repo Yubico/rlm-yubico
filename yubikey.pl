11195586e678d4a7f3685e137183044330c24667
Dain Nilsson <dain@yubico.com>
2013-02-25 09:52:24 +0100

    Cleaned up some code.

--- yubikey.pl
+++ yubikey.pl
@@ -1,19 +1,9 @@
 use strict;
+use warnings;
 use vars qw(%RAD_REQUEST %RAD_REPLY %RAD_CHECK);
 use AnyEvent::Yubico;
 use Crypt::CBC;
 use Error qw(:try);
-
-use constant    RLM_MODULE_REJECT=>    0;#  /* immediately reject the request */
-use constant	RLM_MODULE_FAIL=>      1;#  /* module failed, don't reply */
-use constant	RLM_MODULE_OK=>        2;#  /* the module is OK, continue */
-use constant	RLM_MODULE_HANDLED=>   3;#  /* the module handled the request, so stop. */
-use constant	RLM_MODULE_INVALID=>   4;#  /* the module considers the request invalid. */
-use constant	RLM_MODULE_USERLOCK=>  5;#  /* reject the request (user is locked out) */
-use constant	RLM_MODULE_NOTFOUND=>  6;#  /* user not found */
-use constant	RLM_MODULE_NOOP=>      7;#  /* module succeeded without doing anything */
-use constant	RLM_MODULE_UPDATED=>   8;#  /* OK (pairs modified) */
-use constant	RLM_MODULE_NUMCODES=>  9;#  /* How many return codes there are */
 
 # Default values
 our $id_len = 12;
@@ -26,40 +16,36 @@ our $mapping_file = "/etc/yubico/rlm/ykmapping";
 # Load user configuration
 do "/etc/yubico/rlm/ykrlm-config.cfg";
 
+# Initialization
 my $otp_len = 32 + $id_len;
 
 my $key = Crypt::CBC->random_bytes(128);
 my $cipher = Crypt::CBC->new(
-   -key        => $key,
-   -cipher     => 'Blowfish',
-   -padding    => 'space',
-   -add_header => 1
+	-key => $key,
+	-cipher => 'Blowfish',
+	-padding => 'space',
+	-add_header => 1
 );
 my $ykval = AnyEvent::Yubico->new({
-   client_id   => $client_id,
-   api_key     => $api_key,
-   urls        => $verify_urls
+	client_id => $client_id,
+	api_key => $api_key,
+	urls	=> $verify_urls
 });
 
-# Simple file based YubiKey mapping:
-my $mappingdata = {};
-open(my $info, $mapping_file);
-while(my $line = <$info>) {
-        chomp($line);
-        my ($username, $keystring) = split(/:/, $line, 2);
-        my @keys = split(/,/, $keystring);
-        $mappingdata->{$username} = \@keys;
-}
+########################
+# FreeRADIUS functions #
+########################
 
-sub has {
-        my($username, $key) = @_;
-        foreach my $x (@{$mappingdata->{$username}}) {
-                if( $x eq $key ) {
-                        return 1;
-		}
-        }
-        return 0;
-}
+use constant    RLM_MODULE_REJECT=>    0;#  /* immediately reject the request */
+use constant	RLM_MODULE_FAIL=>      1;#  /* module failed, don't reply */
+use constant	RLM_MODULE_OK=>	2;#  /* the module is OK, continue */
+use constant	RLM_MODULE_HANDLED=>   3;#  /* the module handled the request, so stop. */
+use constant	RLM_MODULE_INVALID=>   4;#  /* the module considers the request invalid. */
+use constant	RLM_MODULE_USERLOCK=>  5;#  /* reject the request (user is locked out) */
+use constant	RLM_MODULE_NOTFOUND=>  6;#  /* user not found */
+use constant	RLM_MODULE_NOOP=>      7;#  /* module succeeded without doing anything */
+use constant	RLM_MODULE_UPDATED=>   8;#  /* OK (pairs modified) */
+use constant	RLM_MODULE_NUMCODES=>  9;#  /* How many return codes there are */
 
 
 # Make sure the user has a valid YubiKey OTP
@@ -110,23 +96,23 @@ sub authorize {
 			return RLM_MODULE_NOOP;
 		}
 	} elsif(validate_otp($otp)) {
-		my $publicId = substr($otp, 0, $id_len);
+		my $public_id = substr($otp, 0, $id_len);
 
 		#Lookup username if needed/allowed.
 		if($username eq '' and $allow_userless_login) {
-			$username = lookup_username($publicId);
+			$username = lookup_username($public_id);
 			$RAD_REQUEST{'User-Name'} = $username;
 		}
 
-		if(otp_belongs_to($publicId, $username)) {
+		if(key_belongs_to($public_id, $username)) {
 			&radiusd::radlog(1, "$username has valid OTP: $otp");
 			return RLM_MODULE_OK;
-		} elsif(can_provision($publicId, $username)) {
-			&radiusd::radlog(1, "Attempt to provision $publicId for $username post authentication");
-			$RAD_CHECK{'YubiKey-Provision'} = $publicId;
+		} elsif(can_provision($public_id, $username)) {
+			&radiusd::radlog(1, "Attempt to provision $public_id for $username post authentication");
+			$RAD_CHECK{'YubiKey-Provision'} = $public_id;
 			return RLM_MODULE_UPDATED;	
 		} else {
-			&radiusd::radlog(1, "Reject: $username using valid OTP from foreign YubiKey: $publicId");
+			&radiusd::radlog(1, "Reject: $username using valid OTP from foreign YubiKey: $public_id");
 			$RAD_REPLY{'Reply-Message'} = "Invalid OTP!";
 			return RLM_MODULE_REJECT;
 		}
@@ -140,16 +126,57 @@ sub authorize {
 
 # Do auto-provisioning, if needed, after authentication.
 sub post_auth {
-	my $publicId = $RAD_CHECK{'YubiKey-Provision'};
+	my $public_id = $RAD_CHECK{'YubiKey-Provision'};
 	my $username = $RAD_REQUEST{'User-Name'};
 
-	if($publicId =~ /^[cbdefghijklnrtuv]{$id_len}$/) {
-		provision($publicId, $username);
+	if($public_id =~ /^[cbdefghijklnrtuv]{$id_len}$/) {
+		provision($public_id, $username);
 	}
 
 	return RLM_MODULE_OK;
 }
 
+##################
+# OTP Validation #
+##################
+
+# Validates a YubiKey OTP.
+sub validate_otp {
+	my($otp) = @_;
+
+	return $ykval->verify($otp);
+}
+
+
+# Encrypts a password using an instance specific key
+sub encrypt_password {
+	my($plaintext) = @_;
+
+	return $cipher->encrypt($plaintext);
+}
+
+# Decrypts a password using an instance specific key
+sub decrypt_password {
+	my($ciphertext) = @_;
+
+	return $cipher->decrypt($ciphertext);
+}
+
+###################
+# YubiKey Mapping #
+###################
+
+# Simple file based YubiKey mapping:
+my $mappingdata = {};
+open(my $info, $mapping_file);
+while(my $line = <$info>) {
+	chomp($line);
+	next if $line =~ /^(#|$)/;
+
+	my ($username, $keystring) = split(/:/, $line, 2);
+	my @keys = split(/,/, $keystring);
+	$mappingdata->{$username} = \@keys;
+}
 
 # Check if a particular username requires an OTP to log in.
 sub requires_otp {
@@ -159,24 +186,21 @@ sub requires_otp {
 	return 1;
 }
 
-# Validates a YubiKey OTP.
-sub validate_otp {
-	my($otp) = @_;
-
-	return $ykval->verify($otp);
-}
-
-# Checks if the given OTP comes from a YubiKey belonging to the 
+# Checks if the given public id comes from a YubiKey belonging to the 
 # given user.
-sub otp_belongs_to {
+sub key_belongs_to {
 	my($public_id, $username) = @_;
-
-	return has($username, $public_id);
+	foreach my $x (@{$mappingdata->{$username}}) {
+		if($x eq $public_id) {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 # Can we auto-provision the given YubiKey for the user?
 sub can_provision {
-	my($publicId, $username) = @_;
+	my($public_id, $username) = @_;
 
 	#TODO: Insert logic for determining if a YubiKey can be provisioned here
 	return 0;
@@ -184,30 +208,16 @@ sub can_provision {
 
 # Provision the given YubiKey to the given user.
 sub provision {
-	my($publicId, $username) = @_;
+	my($public_id, $username) = @_;
 	
 	#TODO: Insert provisioning logic here
-	&radiusd::radlog(1,"Provisioned $publicId to $username");
+	&radiusd::radlog(1,"Provisioned $public_id to $username");
 }
 
 sub lookup_username {
-	my($publicId) = @_;
+	my($public_id) = @_;
 
 	#TODO: Lookup username for OTP
 	return "baduser";
-}
-
-# Encrypts a password using an instance specific key
-sub encrypt_password {
-	my($plaintext) = @_;
-
-	return $cipher->encrypt($plaintext);
-}
-
-#Decrypts a password using an instance specific key
-sub decrypt_password {
-	my($ciphertext) = @_;
-
-	return $cipher->decrypt($ciphertext);
 }
 
